9e8ac6555827d1a8c5457fc5b241e397372cd89b
Space995 <Space995@users.noreply.github.com>
2017-08-01 14:32:05 +0200

    Add 'player-invisibility' flag registration

--- src/main/java/io/github/space995/player_invisibility_flag/PlayerInvisibilityFlag_Plugin.java
+++ src/main/java/io/github/space995/player_invisibility_flag/PlayerInvisibilityFlag_Plugin.java
@@ -1,8 +1,39 @@
 package io.github.space995.player_invisibility_flag;
 
+import com.sk89q.worldguard.bukkit.WGBukkit;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.BooleanFlag;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerInvisibilityFlag_Plugin extends JavaPlugin
 {
-    
+    // BooleanFlag with the name "player-invisibility"
+    public static final Flag PLAYER_INVISIBILITY_FLAG = new BooleanFlag("player-invisibility");
+
+    @Override
+    public void onLoad()
+    {
+        getLogger().info("INFO: Hooking with WorldGuard.");
+
+        final WorldGuardPlugin worldGuardPlugin = WGBukkit.getPlugin();
+        final FlagRegistry registry = worldGuardPlugin.getFlagRegistry();
+
+        getLogger().info("OK: Hooked with WorldGuard.");
+
+        try
+        {
+            // register our flag with the registry
+            registry.register(PLAYER_INVISIBILITY_FLAG);
+            getLogger().info("OK: Registered 'player-invisibility' flag.");
+        }
+        catch (FlagConflictException e)
+        {
+            // print a message to let the server admin know of the conflict
+            getLogger().info("FATAL-ERROR: Some other plugin registered a flag by the name 'player-invisibility'.");
+            getLogger().info("WARNING: This could cause issues with saved flags in region files!");
+        }
+    }
 }
